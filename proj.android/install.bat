f2c53cba4712ca2b3b62976df1d46e3ff2994794
dm <frankinshtein85@gmail.com>
2018-10-26 15:34:57 +0500

    latest oxygine

--- proj.android/install.bat
+++
@@ -1,2 +0,0 @@
-adb install -r bin/Swipeable-debug.apk
-adb shell am start -n org.oxygine.Swipeable/org.oxygine.Swipeable.MainActivity
--- proj.android/install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-adb install -r bin/Swipeable-debug.apk
-adb shell am start -n org.oxygine.Swipeable/org.oxygine.Swipeable.MainActivity
